6ebe364fc58e8d185ec2d53a9e7d4d1369211b2f
mzech <magszech@aim.com>
2016-05-25 17:05:47 -0700

    Delete Codes_DayOfWeekID.sql

--- Codes_DayOfWeekID.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Codes_DayOfWeekID] (
-    [DayOfWeekID] VARCHAR (20) NOT NULL,
-    [Description] VARCHAR (20) NOT NULL,
-    [SortOrder]   INT          NULL,
-    CONSTRAINT [PK_Codes_DayOfWeekID] PRIMARY KEY CLUSTERED ([DayOfWeekID] ASC)
-);
-
